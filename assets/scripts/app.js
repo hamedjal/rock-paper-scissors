aa7674309fbd69022237395b344d03778b0bfcc7
hamedjal <67151213+hamedjal@users.noreply.github.com>
2022-06-05 09:43:37 +0200

    ✄

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
-const PLAY_ROUND = 5;
+let PLAY_ROUND = 5;
 let playerScore = 0;
 let computerScore = 0;
+let result;
+let i;
 let log = {};
 const fullGameLog = [];
 
@@ -12,48 +14,70 @@ const computerPlay = () => {
 
 const playRound = (playerSelection, computerSelection) => {
   const win = { ROCK: "SCISSORS", PAPER: "ROCK", SCISSORS: "PAPER" };
-  let result;
   if (win[playerSelection] === computerSelection) {
     result = (`You Won! ${playerSelection} Beats ${computerSelection}!`);
     log = {
-      id: Math.random().toString(), result: result,
-      computerChoice: computerSelection, computerScore: computerScore,
-      playerChoice: playerSelection, playerScore: playerScore++
+      id: i,
+      result: result,
+      computerChoice: computerSelection,
+      computerScore: computerScore,
+      playerChoice: playerSelection,
+      playerScore: playerScore++
     };
     fullGameLog.push(log);
   } else if (win[computerSelection] === playerSelection) {
     result = (`You Lost! ${computerSelection} Beats ${playerSelection}!`);
     log = {
-      id: Math.random().toString(), result: result,
-      computerChoice: computerSelection, computerScore: computerScore++,
-      playerChoice: playerSelection, playerScore: playerScore
+      id: i,
+      result: result,
+      computerChoice: computerSelection,
+      computerScore: computerScore++,
+      playerChoice: playerSelection,
+      playerScore: playerScore
     };
     fullGameLog.push(log);
   } else {
     result = ("You have a Draw!");
     log = {
-      id: Math.random().toString(), result: result,
-      computerChoice: computerSelection, computerScore: computerScore,
-      playerChoice: playerSelection, playerScore: playerScore
+      id: i,
+      result: result,
+      computerChoice: computerSelection,
+      computerScore: computerScore,
+      playerChoice: playerSelection,
+      playerScore: playerScore
     };
     fullGameLog.push(log);
   }
+  return result;
 }
 
 const game = () => {
-  for (let i = 0; i < PLAY_ROUND; i++) {
-    let playerSelection = window.prompt("Enter your choice:").trim().toUpperCase();
-    if (playerSelection !== "ROCK" || playerSelection !== "PAPER" || playerSelection !== "SCISSORS")
-      playerSelection = computerPlay();
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
+  console.log(`GAME STARTED!`);
+  for (i = 1; i <= PLAY_ROUND; i++) {
+    let playerSelection = window.prompt("Enter your choice: (Rock, Paper, Scissors).");
+    if (playerSelection !== null) {
+      playerSelection = playerSelection.trim().toUpperCase();
+    } else if (playerSelection === null) {
+      playerSelection = 'cancel!';
+    }
+
+    if (playerSelection === "ROCK" || playerSelection === "PAPER" || playerSelection === "SCISSORS") {
+      let computerSelection = computerPlay();
+      playRound(playerSelection, computerSelection);
+      console.log(`Round ${i}: ${result}`);
+    } else {
+      PLAY_ROUND++;
+      if (playerSelection === "") {
+        playerSelection = 'nothing!';
+      }
+      window.alert(`You selected ${playerSelection} !Please enter a correct choice! (Rock, Paper, Scissors).`);
+    }
   }
   console.log("GAME OVER!");
   playerScore > computerScore ? console.log(`YOU WON! Computer Score: ${computerScore} Your Score: ${playerScore}`)
     : playerScore < computerScore ? console.log(`YOU LOST! Computer Score: ${computerScore} Your Score: ${playerScore}`)
       : console.log(`You have a Draw! Computer Score: ${computerScore} Your Score: ${playerScore}`);
-  console.log("FULL GAME LOG:");
-  console.log(fullGameLog);
+  console.log('FULL GAME LOG:\n', fullGameLog);
 }
 
 game();
